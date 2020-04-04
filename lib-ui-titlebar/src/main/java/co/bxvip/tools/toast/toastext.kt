587bee2c75a6dac8d5414d4b7551a79b986c570f
bxvip <38159930+bxvip@users.noreply.github.com>
2020-04-04 17:31:09 +0800

    inline fun Context.toastMe(message: CharSequence) = ToastUtils.show(message)
    inline fun Context.toastMe(message: CharSequence) = ToastUtils.show(message)

--- lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
+++ lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
@@ -3,3 +3,5 @@ package co.bxvip.tools.toast
 import android.app.Activity
 
 inline fun Activity.toastMe(message: CharSequence) = ToastUtils.show(message)
+
+inline fun Context.toastMe(message: CharSequence) = ToastUtils.show(message)
