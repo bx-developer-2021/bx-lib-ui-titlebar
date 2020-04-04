2a359350237f2a25d98115ebdc0cd40b281ca345
bxvipmac <bxvipsky@gmail.com>
2020-04-04 17:49:37 +0800

    add import context

--- lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
+++ lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
@@ -1,7 +1,9 @@
 package co.bxvip.tools.toast
 
 import android.app.Activity
+import android.content.Context
 
 inline fun Activity.toastMe(message: CharSequence) = ToastUtils.show(message)
 
 inline fun Context.toastMe(message: CharSequence) = ToastUtils.show(message)
+
