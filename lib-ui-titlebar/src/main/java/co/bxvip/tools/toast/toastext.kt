15aeb898ef5afff06c8a3d2cfcc48dc1dcc5c559
bxvip2020 <bxdeveloper2020@gmail.com>
2022-01-13 16:19:03 +0800

    feat: 新增 check, 拿掉 inline
    新增 check, 這裡進行最後的 null 以及 empty 檢查, 讓外部呼叫都可以省去這個步驟
    inline 主要是用於參數函數 function type parameters, 其他情況用 inline 的效益不大

--- lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
+++ lib-ui-titlebar/src/main/java/co/bxvip/tools/toast/toastext.kt
@@ -3,7 +3,17 @@ package co.bxvip.tools.toast
 import android.app.Activity
 import android.content.Context
 
-inline fun Activity.toastMe(message: CharSequence) = ToastUtils.show(message)
+@Suppress("unused")
+fun Activity.toastMe(message: CharSequence?) {
+    if (message != null && message.isNotEmpty() && message.isNotBlank()) {
+        ToastUtils.show(message)
+    }
+}
 
-inline fun Context.toastMe(message: CharSequence) = ToastUtils.show(message)
+@Suppress("unused")
+fun Context.toastMe(message: CharSequence?) {
+    if (message != null && message.isNotEmpty() && message.isNotBlank()) {
+        ToastUtils.show(message)
+    }
+}
 
